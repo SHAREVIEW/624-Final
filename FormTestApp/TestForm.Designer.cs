c89d3c1d5d886bbb1d54eba4ed29ad0adcb07862
hainesricky <hainesricky@tamu.edu>
2015-11-20 17:32:21 -0600

    Initial commit
    Basic modified sample project

---
+++ FormTestApp/TestForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FormTestApp
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.scribblePanel = new System.Windows.Forms.Panel();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.BackColor = System.Drawing.Color.Lime;
+            this.testButton.Location = new System.Drawing.Point(12, 12);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "Test...";
+            this.testButton.UseVisualStyleBackColor = false;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearButton.BackColor = System.Drawing.Color.Gray;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(1641, 344);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(160, 100);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // scribblePanel
+            // 
+            this.scribblePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scribblePanel.BackColor = System.Drawing.Color.Gainsboro;
+            this.scribblePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scribblePanel.Location = new System.Drawing.Point(12, 43);
+            this.scribblePanel.Name = "scribblePanel";
+            this.scribblePanel.Size = new System.Drawing.Size(1789, 756);
+            this.scribblePanel.TabIndex = 3;
+            this.scribblePanel.Resize += new System.EventHandler(this.scribblePanel_Resize);
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(88, 17);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(162, 13);
+            this.testLabel.TabIndex = 6;
+            this.testLabel.Text = "Press Test button to start testing.";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1813, 811);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.scribblePanel);
+            this.Controls.Add(this.testButton);
+            this.Name = "TestForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Panel scribblePanel;
+        private System.Windows.Forms.Label testLabel;
+    }
+}
+
